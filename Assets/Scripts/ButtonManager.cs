e14731de9055534984fc872a554d43de6a22051e
Nikunj <nikunjsaraf2001@gmail.com>
2020-12-06 12:48:07 +0530

    Pause Menu

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
 {
+    [SerializeField] GameObject pausePanel = null;
+
+    private void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
     public void ReplayButton()
     {
 
@@ -17,5 +24,17 @@ public class ButtonManager : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
     }
 }
